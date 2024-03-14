fd4d24ac41409a5677437e3494df143c4d6ff215
Tada <tada@MacBook-Pro-khxng-Tada.local>
2024-03-14 21:51:26 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  output:'export',
+  distDir: 'dist',
+  basePath: '/randomcat_evo',
+  assetPrefix: '/randomcat_evo/',
+};
   
   export default nextConfig;
